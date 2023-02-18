535becade0f4f01d54dfea0889ad177ca351ed49
Эржигит Имамов <e.imamov@glory.bet>
2023-02-18 18:04:39 +0600

    feat: add start:dev via webpack

---
+++ webpack-hmr.config.js
@@ -0,0 +1,22 @@
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      process.env.NODE_ENV === 'development' ? new RunScriptWebpackPlugin({ name: options.output.filename, autoRestart: false }) : undefined,
+    ],
+  };
+};
